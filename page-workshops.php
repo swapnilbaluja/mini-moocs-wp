fe9864e45e27baac7832b5dbe3af768553ae49ba
ArpitX <arpit.bansal2010@gmail.com>
2017-09-05 00:41:03 +0530

    carousel

---
+++ page-workshops.php
@@ -0,0 +1,32 @@
+<?php get_header(); ?>
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xs-12 col-md-8">
+					<main id="main" class="site-main" role="main">
+						<?php
+						while ( have_posts() ) : the_post();
+
+							get_template_part( 'template-parts/page/content', 'page' );
+
+							// If comments are open or we have at least one comment, load up the comment template.
+							if ( comments_open() || get_comments_number() ) :
+								comments_template();
+							endif;
+
+						endwhile; // End of the loop.
+						?>
+
+					</main><!-- #main -->
+				</div>
+				<div class="col-xs-12 col-md-4">
+					<?php get_sidebar(); ?>
+				</div>
+			</div>
+		</div>
+		
+	</div><!-- #primary -->
+</div><!-- .wrap -->
+
+<?php get_footer();
